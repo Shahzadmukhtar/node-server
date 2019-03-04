adc2e9061d74ff03af3afc08e04daecbe6aa1727
Shahzad <contact.shahzadmukhtar@gmail.com>
2019-03-04 06:36:31 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var hbs = require('hbs');
+
+var app = express();
+
+app.set('view engine' , hbs);
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partial');
+
+hbs.registerHelper("getcurrentyear" , () => {
+return new Date().getFullYear();
+});
+app.use((req, res, next) =>
+{
+    res.render('mantance.hbs');
+});
+
+hbs.registerHelper("convert" , (text) => {
+return text.toUpperCase();
+});
+
+app.get('/',(req , res) => {
+    res.render('home.hbs' , {
+        pagetitle : "welcome to home",
+    });
+});
+
+app.get('/bad' , (req , res) => {
+
+res.send({
+message: "page not found"
+});
+
+});
+app.listen(3000);
